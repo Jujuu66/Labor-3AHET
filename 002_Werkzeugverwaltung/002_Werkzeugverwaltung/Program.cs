2e685462079f97a3f1e5db93a0b8c6c01873284c
Julian Salfelner <julian.salfelner@htlwy.at>
2026-09-23 15:44:28 +0200

    Funktionierend

--- 002_Werkzeugverwaltung/002_Werkzeugverwaltung/Program.cs
+++ 002_Werkzeugverwaltung/002_Werkzeugverwaltung/Program.cs
@@ -12,7 +12,7 @@ namespace _002_Werkzeugverwaltung
         // Member-Variablen (eigenschaften)
         private string description;
         private int number;
-        private bool inStore;
+        private bool inStore = true;
 
         // Methoden (Unterprogrammen)
 
@@ -34,10 +34,10 @@ namespace _002_Werkzeugverwaltung
 
         }
 
-        public void setinStore(bool inStore)
+        public int getnumber()
         {
 
-            this.inStore = inStore;
+           return number;
 
 
 
@@ -50,6 +50,23 @@ namespace _002_Werkzeugverwaltung
         
         }
 
+        public void SetFalse()
+        {
+
+
+            inStore = false;
+
+
+        }
+
+        public void SetTrue()
+        {
+
+
+            inStore = true;
+
+
+        }
     }
 
     internal class Program
@@ -57,25 +74,29 @@ namespace _002_Werkzeugverwaltung
         static void Main(string[] args)
         {
 
+            List<tool> tools = new List<tool> ();
+            tools.Add (new tool ());
+            tools.Add(new tool());
+            tools.Add(new tool());
 
 
 
-            tool s1 = new tool();
-            tool s2 = new tool();
-            tool s3 = new tool();
+            tools[0].setDescription ("Schraubendreher");
+            tools[0].setnumber(67676);
+         
 
-            s1.setDescription ("Schraubendreher");
-            s1.setnumber(67676);
-            s1.setinStore(true);
+            tools[1].setDescription("Schraubendreher");
+            tools[1].setnumber(12345);
+          
 
-            s2.setDescription("Schraubendreher");
-            s2.setnumber(12345);
-            s2.setinStore(true);
+            tools[2].setDescription("Wasserwaage");
+            tools[2].setnumber(50005);
+           
 
-            s3.setDescription("Wasserwaage");
-            s3.setnumber(50005);
-            s3.setinStore(true);
+            
 
+
+            int i = 3;
             bool stayAlive = true;
             while (stayAlive)
             {
@@ -89,7 +110,7 @@ namespace _002_Werkzeugverwaltung
                 char eingabe = Console.ReadKey().KeyChar;
 
 
-
+               Console.Clear();
 
                 switch (eingabe)
                 {
@@ -98,18 +119,64 @@ namespace _002_Werkzeugverwaltung
                         break;
 
                     case 'l':
+                        foreach (tool tool in tools)
+                        {
 
+                            tool.Print();
+                        }
                         break;
 
                     case 'h':
+
+                        Console.WriteLine("Welches Werkzeug wird hinzugefügt");
+                       string stringHinzu = Console.ReadLine();
+                        Console.WriteLine("Gib die Seriennummer ein");
+                        int numberHinzu = int.Parse(Console.ReadLine());
+
+                        tools.Add(new tool());
+                        tools[i].setDescription(stringHinzu);
+                        tools[i].setnumber(numberHinzu);
+                        i++;
+
 
                         break;
 
                     case 'a':
 
+                        Console.WriteLine("Seriennummer von den Werkstück das sie ausleihen wollen");
+                        int aus = int.Parse(Console.ReadLine());
+
+
+                        foreach (tool tool in tools)
+                        {
+                            if (aus == tool.getnumber())
+                            {
+                                tool.SetFalse();
+                            
+                            }
+                        }
                         break;
 
+
+
+                       
+
                     case 'r':
+
+                        Console.WriteLine("Seriennummer von den Werkstück das sie zurückgeben wollen");
+                        int Rück = int.Parse(Console.ReadLine());
+
+
+                        foreach (tool tool in tools)
+                        {
+                            if (Rück == tool.getnumber())
+                            {
+                                tool.SetTrue();
+
+                            }
+                        }
+
+
 
                         break;
 
